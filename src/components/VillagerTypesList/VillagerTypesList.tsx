5d98469470cbe76f6518f1ada0266be1b62f79bd
BriBourassa <111149043+BriBourassa@users.noreply.github.com>
2023-06-09 16:52:59 -0600

    Display all 35 species on the main page

--- src/components/VillagerTypesList/VillagerTypesList.tsx
+++ src/components/VillagerTypesList/VillagerTypesList.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import App from '../App/App';
+import Villager from '../../types/Villager';
+import './VillagerTypesList.css'
 
-export interface IAppProps {
+export interface Props {
+  allVillagers: Villager[];
 }
 
 // display the main page list of 35 species
 
-function VillagerTypesList (props: IAppProps) {
-  return (
-    <div>
-      
-    </div>
-  );
-}
+const VillagerTypesList: React.FC<Props> = ({ allVillagers }) => {
+  const displaySpeciesList = () => {
+    const display = Object.keys(allVillagers).map((species) => {
+      return (
+        <Link to={`/${species}`} key={species}>
+          <div className="single-species">
+            <img src={allVillagers[species][0].iconUri} alt={species}/>
+            <h1>{species}</h1>
+          </div>
+        </Link>
+      );
+    });
+    console.log(display)
+    return display;
+  };
 
-export default VillagerTypesList
+  return <div>{displaySpeciesList()}</div>;
+};
+
+export default VillagerTypesList;
